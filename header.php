80a91bf47295e8f176b7a7fd14cd70840605bb74
stepandrew <46725756+stepandrew@users.noreply.github.com>
2019-09-28 00:24:22 -0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+
+<head>
+  
+    <link href="style.css" type="text/css" rel="stylesheet">
+
+
+    
+</head>
+<body>
+    <a href="home.php">
+    	<div class="logo" >
+	</div>
+	</a>
+		<h2 id="title">SarangFamily</h2>
+
+
+ 	  <ul class = "signupinForm">
+            <li>
+                    <a href = "login.php">LOG IN</a>
+
+            </li>
+            <li>
+                    <a href = "logout.php">LOG OUT</a>
+            </li>
+            <li>
+                    <a href = "signup.php">REGISTER</a>
+            </li>
+          </ul>	
+
+
+
+	<div clss="connection">
+	  <ul>
+		<a href = "home.php">
+		<a href= "receipe.php">
+<a href= " ">
+<a href=" ">
+<a href= " ">
+<a href= " ">
+
+          </ul>
+	</div>
+
+
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,44 @@
+
+<head>
+
+
+
+    	<link rel="shortcut icon" href="flag32.ico" type="image/x-icon">
+
+</head>
+<body>
+  <div class="header-container">
+
+	<?php
+  	  require_once('header.php');
+	?>
+  <div class="body-container">
+    	<h2>Welcome to SarangFamily</h2>
+
+ 	<button class="body-button" id="menu-button">home</button>
+    <button class="body-button" id="login-button">login</button>
+    <button class="body-button" id="logout-button">logout</button>
+    <button class="body-button" id="signup-button">signup</button>
+
+
+
+
+    </div>
+  </div>
+
+    <div class="footer-container">
+
+	<?php
+ 	   require_once('footer.php');
+	?>
+    </div>
+
+
+
+
+</body>
+
+
+
+
+</html>
