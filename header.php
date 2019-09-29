a46ec135434afc6d5231f548d04162d334c5c1e8
stepandrew <46725756+stepandrew@users.noreply.github.com>
2019-09-29 12:25:12 -0600

    Add files via upload

--- header.php
+++ header.php
@@ -6,43 +6,31 @@
   
     <link href="style.css" type="text/css" rel="stylesheet">
 
-
-    
 </head>
 <body>
     <a href="home.php">
     	<div class="logo" >
 	</div>
 	</a>
+	 <a href="home.php">
 		<h2 id="title">SarangFamily</h2>
-
+    </a>
 
  	  <ul class = "signupinForm">
             <li>
                     <a href = "login.php">LOG IN</a>
-
             </li>
             <li>
                     <a href = "logout.php">LOG OUT</a>
             </li>
             <li>
-                    <a href = "signup.php">REGISTER</a>
+                    <a href = "register.php">REGISTER</a>
             </li>
           </ul>	
 
 
 
-	<div clss="connection">
-	  <ul>
-		<a href = "home.php">
-		<a href= "receipe.php">
-<a href= " ">
-<a href=" ">
-<a href= " ">
-<a href= " ">
 
-          </ul>
-	</div>
 
 
 </body>
