8b83a06bc0303acfa23b3145b5abbaebb871b638
stepandrew <46725756+stepandrew@users.noreply.github.com>
2019-09-29 23:39:10 -0600

    Add files via upload

---
+++ register-handler.php
@@ -0,0 +1,9 @@
+<html>
+ <body>
+<!--
+ First name : <?php echo $_POST["first name"]; ?><br>
+ Last name : <?php echo $_POST["last name"]; ?>
+-->
+This is register-handler page.
+ </body>
+ </html>
