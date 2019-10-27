92c3f48ba09a56e83ee04ae483e50fa9b8dbab53
stepandrew <46725756+stepandrew@users.noreply.github.com>
2019-10-27 12:22:41 -0600

    Add files via upload

--- register-handler.php
+++ register-handler.php
@@ -1,9 +1,44 @@
+<?php
+session_start();
+setcookie("Hi", "cookie");
+echo print_r($_COOKIE, 1);
+?>
+
 <html>
  <body>
-<!--
- First name : <?php echo $_POST["first name"]; ?><br>
- Last name : <?php echo $_POST["last name"]; ?>
--->
-This is register-handler page.
+
+  <?php 
+	 echo print_r($_POST,1);
+	 
+	 $firstName = htmlspecialchars(trim($_POST["firstName"])); 
+	 $lastName = htmlspecialchars(trim($_POST["lastName"])); 
+	 $email = trim($_POST["email"]);
+	 $password = trim($_POST["password"]);
+	 $password_match = trim($_POST["password_match"]);
+	 
+	 if(strlen($firstName) <=0 || strlen($firstName)>50){ 
+		 $firstNameError = "first name is required. Must be less than 50 characters.";
+	}
+	 
+	  if(strlen($email) <=0 || strlen($email)>50){ 
+		 $emailError = "email is required. Must be less than 50 characters.";
+	}else if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		  $emailError = "Must be valid email address.";
+		  
+	  }
+	 ?>
+	 
+	<p> First Name: <?php echo htmlspecialchars($firstName) ?></p>
+		 <?php if(isset($firstNameError)){ ?>
+			<span id="firstNameError" class="error"> <?php $firstNameError ?></span>
+		 <?php } ?>
+
+	<p> Last Name: <?php echo htmlspecialchars($lastName) ?></p>
+
+	<p> Email: <?php echo htmlspecialchars($email); ?></p>
+		  <?php if(isset($emailError)){ ?>
+			<span id="emailError" class="error"> <?php $emailError ?></span>
+		 <?php } ?>
+	<p> Password Match: <?php echo htmlspecialchars($password_match) ?></p>
  </body>
  </html>
--- register.php
+++ register.php
@@ -1,28 +1,41 @@
 ﻿
 <html>
- <div class="header-container">
-
+ <body>
+	 <div class="header-container">
           	<?php
             	  require_once('header.php');
           	?>
-  </div>
-
- <body>
+	 </div>
     <div id = "register">
-        <h2>Register here Saranfamily homepage<h2>
-        <h2>This page is under construction<h2>
+        <h2>Register here Saranfamily homepage<h2>        
     </div>
-    <div>
+<section>
+   
     <form action="register-handler.php" method ="post">
+		<fieldset>
+		
+		
         First name:<br>
-        <input type = "text" name = "first name" value="Anna"/>
-        <br>
+        	<input type = "text" id ="firstName" name = "firstName" maxlength="50" required/>
+       		 <br><br>
         Last name: <br>
-        <input type = "text" name="last name" value ="Han"/>
-        <br>
-        <input type="radio" name="gender" value="male" checked> Male<br>
+      	  	<input type = "text" id= "lastName" name="lastName" required/>
+			<br><br>
+		
+	    email:<br>
+            <input type = "email" id="email" name = "email" required />
+            <br><br>
+		
+       password: <br>
+            <input type = "password" id = "password" name="password" required />
+            <br><br>
+	
+		 password match: <br>
+            <input type = "password" id= "password_match" name="password_match" required />
+            <br><br>
+   <!--	<input type="radio" name="gender" value="male" checked> Male<br>
           <input type="radio" name="gender" value="female"> Female<br>
-          <input type="radio" name="gender" value="other"> Other<br><br>
+          <input type="radio" name="gender" value="other"> Other<br><br> -->
         Birthday:
         <input type="date" name="bday">
 
@@ -30,15 +43,14 @@
         <br>
         <input type="submit" value ="Submit"/>
         <input type="reset">
-
+		</fieldset>
      </form>
 
-</div>
-
+</section>
   <div class="footer-container">
 
 	<?php
  	   require_once('footer.php');
 	?>
     </div>
-</html
+</html>
